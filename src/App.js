07f1d56964759a892a9e8b568070afa42c6f47c6
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-07-07 10:15:39 +0600

    nav1 done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar1 from './Components/Navbar1';
 
 function App() {
   return (
     <div className="App">
-     <div className="card w-96 bg-base-100 shadow-xl">
-  <figure className="px-10 pt-10">
-    <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
-  </figure>
-  <div className="card-body items-center text-center">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+    <Navbar1></Navbar1>
     </div>
   );
 }
